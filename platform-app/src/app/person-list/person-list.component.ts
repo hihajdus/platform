6c1f0087a240eb398eab084861b8364bcf0a8348
hihajdus <sebastian.hajdus@gmail.com>
2020-02-18 22:57:39 +0100

    mocked API, show person list

--- platform-app/src/app/person-list/person-list.component.ts
+++ platform-app/src/app/person-list/person-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { tap, repeat } from 'rxjs/operators';
 
 @Component({
   selector: 'app-person-list',
   templateUrl: './person-list.component.html',
   styleUrls: ['./person-list.component.scss']
 })
-export class PersonListComponent implements OnInit {
+export class PersonListComponent {
+  persons$: Observable<any> = this.get();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
+  public get() {
+    let id = 0;
+
+    return this.http.get(`/api/person`)
+      .pipe(
+        tap(() => {
+          id += 1;
+          console.log(id);
+        }),
+        repeat(5)
+      );
   }
-
 }
