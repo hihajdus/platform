e8ed6a2af1e02b0765b5d0d1d2e2f738eb35f637
hihajdus <sebastian.hajdus@gmail.com>
2020-02-21 13:53:00 +0100

    add type person

--- platform-app/src/app/services/persons.service.ts
+++ platform-app/src/app/services/persons.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap, repeat } from 'rxjs/operators';
+import { Person } from '../interfaces/person';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +10,17 @@ import { tap, repeat } from 'rxjs/operators';
 export class PersonsService {
   constructor(private http: HttpClient) { }
 
-  getPersons() {
+  getPersons(): Observable<Person[]> {
     let id = 0;
-    return this.http.get(`/api/person`)
-      .pipe(
-        tap(() => {
-          id += 1;
-          console.log(id);
-        }),
-        repeat(5)
-      );
+    return this.http.get<Person[]>(`/api/person`)
+        .pipe(
+          tap(() => {
+            id += 1;
+            console.log(id);
+          }),
+          repeat(5)
+        );
+
   }
 
   getPerson(id){
