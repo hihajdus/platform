65b182a47d7588bcd13c01b8739cc2bbf482b3ab
hihajdus <sebastian.hajdus@gmail.com>
2020-02-26 14:47:35 +0100

    login test configuration, install jest

--- platform-app/src/app/login/login.component.spec.ts
+++ platform-app/src/app/login/login.component.spec.ts
@@ -1,3 +1,4 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
@@ -8,9 +9,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [ LoginComponent ],
+      imports: [ ReactiveFormsModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,15 +20,44 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
+  const email = component.profileForm.controls.email;
+  const password = component.profileForm.controls.password;
+  const element = fixture.nativeElement;
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
   it('should render form with email and password inputs', () => {
-    const element = fixture.nativeElement;
-
     expect(element.querySelector('form').toBeTruthy());
     expect(element.querySelector('#email').toBeTruthy());
     expect(element.querySelector('#password').toBeTruthy());
     expect(element.querySelector('button').toBeTruthy());
+  })
+  it('should return model invalid when form is empty', () => {
+    expect(component.profileForm.valid).toBeFalsy();
+  });
+  it('should validate email input as required', () => {
+    expect(email.valid).toBeFalsy();
+    expect(email.errors.required).toBeTruthy();
+  });
+  it("should validate password input as required", () => {
+    expect(password.valid).toBeFalsy();
+    expect(password.errors.required).toBeTruthy();
+  });
+  it('should validate email format', () => {
+    email.setValue('test');
+    const errors = email.errors;
+
+    expect(errors.required).toBeFalsy();
+    expect(errors.required).toBeTruthy();
+    expect(email.valid).toBeFalsy();
+  });
+  it('should validate email format correctly', () => {
+    email.setValue('email@gmai.com');
+    const errors = email.errors || {};
+
+    expect(errors.required).toBeFalsy();
+    expect(errors.pattern).toBeFalsy();
+    expect(email.valid).toBeTruthy();
   })
 });
